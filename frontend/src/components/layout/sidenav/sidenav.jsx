b3cc3f023faccc82e7fa1479360ea94c1e296410
Manish Tripathi <manish82644@gmail.com>
2024-09-12 01:25:38 +0530

    Refactor sidenav component and update menu items

--- frontend/src/components/layout/sidenav/sidenav.jsx
+++ frontend/src/components/layout/sidenav/sidenav.jsx
@@ -1,160 +1,67 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import warriorImage from '../../../assets/images/landscape/man-warrior.png'
+import avatarImage from '../../../assets/images/square/animal-orange-bird.png'
+
+const menuItems = {
+    Game: [{ name: 'Tic Tac Toe', url: '/games/tic-tac-toe' }],
+    Shadows: [{ name: 'Shadows Grid', url: '/shadows' }],
+}
 
 const Sidenav = () => {
     return (
         <div className="relative h-[700px]">
-            <aside className="group/sidebar fixed inset-y-0 left-0 z-40 m-0 flex w-[250px] shrink-0 flex-col overflow-hidden rounded-lg border-r border-dashed border-r-neutral-600 bg-secondary transition-all duration-300 ease-in-out lg:w-[300px]">
+            <aside className="group/sidebar bg-secondary fixed inset-y-0 left-0 z-40 m-0 flex w-[250px] shrink-0 flex-col overflow-hidden rounded-lg border-r border-dashed border-r-light-primary transition-all duration-300 ease-in-out lg:w-[300px] dark:border-dark-primary">
                 <div className="flex h-28 w-full shrink-0 items-center justify-between">
                     <a className="h-full w-full transition-colors duration-200 ease-in-out" href="#">
-                        <img
-                            className="size-full overflow-hidden object-cover object-center"
-                            src="https://ui-library.test/images/landscape/fantasy-scene-gearworks.png"
-                            alt=""
-                        />
+                        <img className="size-full overflow-hidden object-cover object-center" src={warriorImage} alt="Warrior" />
                     </a>
                 </div>
 
-                <div className="border-b border-dashed border-neutral-600 lg:block"></div>
+                <div className="border-b border-dashed border-light-primary lg:block dark:border-dark-primary"></div>
 
-                <div className="flex items-center justify-between px-8 py-5">
-                    <div className="mr-5 flex items-center">
-                        <div className="mr-5">
-                            <div className="relative inline-block shrink-0 cursor-pointer">
-                                <img
-                                    className="inline-block size-11 shrink-0 rounded-lg"
-                                    src="https://ui-library.test/images/square/animal-orange-bird.png"
-                                    alt="Avatar Image"
-                                />
+                <div className="flex items-center justify-between gap-2 px-4 py-5 md:gap-5 md:px-6">
+                    <div className="inline-flex items-start gap-2 md:gap-5">
+                        <div className="block shrink-0">
+                            <div className="cursor-pointer">
+                                <img className="size-11 rounded-lg" src={avatarImage} alt="Avatar Image" />
                             </div>
                         </div>
                         <div className="mr-2">
-                            <a
-                                className="font-medium text-white transition-colors duration-200 ease-in-out hover:text-light-primary"
-                                href="javascript:void(0)">
+                            <a className="text-primary text-base font-medium" href="javascript:void(0)">
                                 Roronoa Zoro
                             </a>
-                            <span className="block text-xs font-medium text-gray-400">Sword Master</span>
+                            <span className="text-secondary block text-xs font-medium">Sword Master</span>
                         </div>
                     </div>
                     <a
-                        className="group relative inline-flex cursor-pointer items-center justify-end border-0 bg-transparent text-center align-middle text-base font-medium leading-normal text-white shadow-none transition-colors duration-150 ease-in-out"
+                        className="text-secondary relative inline-flex cursor-pointer items-center justify-end border-0 text-center align-middle text-base font-medium transition-colors duration-150 ease-in-out"
                         href="javascript:void(0)">
-                        <span className="mr-2 inline-block text-sm text-gray-400">Logout</span>
+                        <span className="mr-2 inline-block text-sm">Logout</span>
                     </a>
                 </div>
 
-                <div className="border-b border-dashed border-neutral-600 lg:block"></div>
+                <div className="border-b border-dashed border-light-primary lg:block dark:border-dark-primary"></div>
 
-                <div className="scrollbar-thin relative my-5 overflow-y-scroll pl-3">
+                <div className="scrollbar-thin relative my-5 overflow-y-scroll md:pl-3">
                     <div className="flex w-full flex-col font-medium">
-                        <div className="block px-4 pb-2">
-                            <span className="text-secondary-dark font-semibold uppercase text-gray-500">Profile</span>
-                        </div>
+                        {Object.keys(menuItems).map((key, index) => (
+                            <div key={index} className="block px-4 pb-2">
+                                {/* Section Heading */}
+                                <span className="text-secondary-dark text-primary font-semibold uppercase">{key}</span>
 
-                        <div>
-                            <span className="my-1 flex cursor-pointer select-none items-center px-4 py-2">
-                                <a className="flex flex-grow items-center text-sm text-gray-400 hover:text-light-primary" href="javascript:;">
-                                    Profile
-                                </a>
-                            </span>
-                        </div>
-
-                        <div>
-                            <span className="my-1 flex cursor-pointer select-none items-center px-4 py-2">
-                                <a className="flex flex-grow items-center text-sm text-gray-400 hover:text-light-primary" href="javascript:;">
-                                    Settings
-                                </a>
-                            </span>
-                        </div>
-                        <div className="block px-4 pb-2 pt-5">
-                            <span className="text-secondary-dark font-semibold uppercase text-gray-500">Applications</span>
-                        </div>
-
-                        <div>
-                            <span className="my-1 flex cursor-pointer select-none items-center px-4 py-2">
-                                <a className="flex flex-grow items-center text-sm text-gray-400 hover:text-light-primary" href="javascript:;">
-                                    Users
-                                </a>
-                            </span>
-                        </div>
-
-                        <div>
-                            <span className="my-1 flex cursor-pointer select-none items-center px-4 py-2">
-                                <a className="flex flex-grow items-center text-sm text-gray-400 hover:text-light-primary" href="javascript:;">
-                                    Orders
-                                </a>
-                            </span>
-                        </div>
-
-                        <div>
-                            <span className="my-1 flex cursor-pointer select-none items-center px-4 py-2">
-                                <a className="flex flex-grow items-center text-sm text-gray-400 hover:text-light-primary" href="javascript:;">
-                                    Track Orders
-                                </a>
-                            </span>
-                        </div>
-
-                        <div>
-                            <span className="my-1 flex cursor-pointer select-none items-center px-4 py-2">
-                                <a className="flex flex-grow items-center text-sm text-gray-400 hover:text-light-primary" href="javascript:;">
-                                    Products
-                                </a>
-                            </span>
-                        </div>
-                        <div className="block px-4 pb-2 pt-5">
-                            <span className="text-secondary-dark font-semibold uppercase text-gray-500">Reports</span>
-                        </div>
-
-                        <div>
-                            <span className="my-1 flex cursor-pointer select-none items-center px-4 py-2">
-                                <a className="flex flex-grow items-center text-sm text-gray-400 hover:text-light-primary" href="javascript:;">
-                                    Sales
-                                </a>
-                            </span>
-                        </div>
-
-                        <div>
-                            <span className="my-1 flex cursor-pointer select-none items-center px-4 py-2">
-                                <a className="flex flex-grow items-center text-sm text-gray-400 hover:text-light-primary" href="javascript:;">
-                                    Expenses
-                                </a>
-                            </span>
-                        </div>
-
-                        <div>
-                            <span className="my-1 flex cursor-pointer select-none items-center px-4 py-2">
-                                <a className="flex flex-grow items-center text-sm text-gray-400 hover:text-light-primary" href="javascript:;">
-                                    Profit
-                                </a>
-                            </span>
-                        </div>
-                        <div className="block px-4 pb-2 pt-5">
-                            <span className="text-secondary-dark font-semibold uppercase text-gray-500">Settings</span>
-                        </div>
-
-                        <div>
-                            <span className="my-1 flex cursor-pointer select-none items-center px-4 py-2">
-                                <a className="flex flex-grow items-center text-sm text-gray-400 hover:text-light-primary" href="javascript:;">
-                                    General
-                                </a>
-                            </span>
-                        </div>
-
-                        <div>
-                            <span className="my-1 flex cursor-pointer select-none items-center px-4 py-2">
-                                <a className="flex flex-grow items-center text-sm text-gray-400 hover:text-light-primary" href="javascript:;">
-                                    Security
-                                </a>
-                            </span>
-                        </div>
-
-                        <div>
-                            <span className="my-1 flex cursor-pointer select-none items-center px-4 py-2">
-                                <a className="flex flex-grow items-center text-sm text-gray-400 hover:text-light-primary" href="javascript:;">
-                                    Notifications
-                                </a>
-                            </span>
-                        </div>
+                                {/* Sub Links */}
+                                {menuItems[key].map((item, itemIndex) => (
+                                    <div key={itemIndex} className="my-1 flex cursor-pointer select-none items-center py-2">
+                                        <Link
+                                            className="text-secondary flex flex-grow items-center text-sm hover:text-dark-primary dark:hover:text-light-primary"
+                                            to={item.url}>
+                                            {item.name}
+                                        </Link>
+                                    </div>
+                                ))}
+                            </div>
+                        ))}
                     </div>
                 </div>
             </aside>
