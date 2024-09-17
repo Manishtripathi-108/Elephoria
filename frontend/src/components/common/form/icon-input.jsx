d24afff6e940ab7d518ca9235beb2680eaa6ff2e
Manish Tripathi <manish82644@gmail.com>
2024-09-18 02:45:05 +0530

    Refactor project structure and add login functionality

---
+++ frontend/src/components/common/form/icon-input.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import Heart from '../../../assets/svgs/heart'
+
+const IconInput = ({ type, placeholder, value, onChange, name, id, error, groupClass, inputClass }) => {
+    const [showPassword, setShowPassword] = React.useState(false)
+
+    return (
+        <div className={`neu-form-group ${groupClass && groupClass}`}>
+            <label className="neu-form-label" htmlFor={id}>
+                {placeholder}
+            </label>
+            <div className={`neu-input-group neu-input-group-prepend ${error && 'error'}`}>
+                {type === 'password' ? (
+                    <span className="neu-input-icon" onClick={() => setShowPassword((prev) => !prev)}>
+                        {showPassword ? '🙈' : '👁️'}
+                    </span>
+                ) : (
+                    <Heart className={`neu-input-icon ${error && 'error'}`} />
+                )}
+                <input
+                    className={`neu-form-input ${inputClass && inputClass}`}
+                    type={showPassword ? 'text' : type}
+                    placeholder={placeholder}
+                    value={value}
+                    onChange={onChange}
+                    name={name}
+                    id={id}
+                />
+            </div>
+            {error && <small className="neu-form-text neu-form-text-error">{error}</small>}
+        </div>
+    )
+}
+
+export default IconInput
--- frontend/src/pages/auth/login.jsx
+++ frontend/src/pages/auth/login.jsx
@@ -1,69 +1,121 @@
-// src/components/Login.js
-import { ErrorMessage, Field, Form, Formik } from 'formik'
-import React from 'react'
-import { Link } from 'react-router-dom'
-import * as Yup from 'yup'
+import React, { useRef, useState } from 'react'
 
-const Login = () => {
-    const initialValues = { email: '', password: '' }
+import Heart from '../../assets/svgs/heart'
+import IconInput from '../../components/common/form/icon-input'
 
-    const validationSchema = Yup.object({
-        email: Yup.string().email('Invalid email format').required('Email is required'),
-        password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
-    })
+// Assuming you have the relevant CSS for styles
 
-    const handleSubmit = (values) => {
-        alert(JSON.stringify(values, null, 2))
+function Login() {
+    const switchContainerRef = useRef(null)
+    const registerContainerRef = useRef(null)
+    const loginContainerRef = useRef(null)
+    const switchSignInRef = useRef(null)
+    const switchSignUpRef = useRef(null)
+
+    const toggleForm = () => {
+        const switchContainer = switchContainerRef.current
+        const registerContainer = registerContainerRef.current
+        const loginContainer = loginContainerRef.current
+        const switchSignIn = switchSignInRef.current
+        const switchSignUp = switchSignUpRef.current
+
+        // Add animation
+        switchContainer.classList.add('animate-auth-slider')
+        setTimeout(() => {
+            switchContainer.classList.remove('animate-auth-slider')
+        }, 1500)
+
+        // Toggle classes for animations
+        switchContainer.classList.toggle('left-[60%]')
+        ;[...switchContainer.querySelectorAll('.switch-circles')].forEach((circle) => {
+            circle.classList.toggle('left-[60%]')
+        })
+
+        switchSignIn.classList.toggle('invisible')
+        switchSignIn.classList.toggle('opacity-0')
+        switchSignUp.classList.toggle('invisible')
+        switchSignUp.classList.toggle('opacity-0')
+
+        registerContainer.classList.toggle('left-0')
+        registerContainer.classList.toggle('left-[40%]')
+        registerContainer.classList.toggle('z-[200]')
+        loginContainer.classList.toggle('left-0')
+        loginContainer.classList.toggle('left-[40%]')
     }
 
     return (
-        <div className="flex justify-center items-center min-h-screen bg-gray-100">
-            <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-md">
-                <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
+        <div className="grid place-items-center h-dvh w-dvw">
+            <div className="bg-primary relative h-[600px] w-[70%] overflow-hidden rounded-xl p-6 shadow-neu-light-lg dark:shadow-neu-dark-lg">
+                {/* Register Container */}
+                <div
+                    className="bg-primary absolute left-[40%] top-0 z-0 flex h-full w-3/5 items-center justify-center p-6 transition-all duration-[1.25s]"
+                    ref={registerContainerRef}>
+                    <form className="flex h-full w-full flex-col items-center justify-center" method="post" action="/register">
+                        <h2 className="form_title text-primary mb-8 text-3xl font-bold">Create Account</h2>
+                        <div className="flex-center text-primary gap-5">{/* Social Icons */}</div>
+                        <span className="text-secondary mb-3 mt-8">or use email for registration</span>
+                        <IconInput groupClass="my-2 w-3/4" type="text" placeholder="Name" />
+                        <IconInput groupClass="my-2 w-3/4" type="email" placeholder="Email" />
+                        <IconInput groupClass="my-2 w-3/4" type="password" placeholder="Password" />
+                        <button className="neu-btn mt-12" type="button">
+                            SIGN UP
+                        </button>
+                    </form>
+                </div>
 
-                <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
-                    {() => (
-                        <Form className="space-y-6">
-                            <div>
-                                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                                    Email
-                                </label>
-                                <Field
-                                    type="email"
-                                    id="email"
-                                    name="email"
-                                    className="mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
-                                />
-                                <ErrorMessage name="email" component="div" className="text-red-500 text-sm mt-1" />
-                            </div>
+                {/* Login Container */}
+                <div
+                    className="bg-primary absolute left-[40%] top-0 z-[100] flex h-full w-3/5 items-center justify-center p-6 transition-all duration-[1.25s]"
+                    ref={loginContainerRef}>
+                    <form className="flex h-full w-full flex-col items-center justify-center" method="post" action="/login">
+                        <h2 className="form_title text-primary mb-8 text-3xl font-bold">Sign in to Website</h2>
+                        <div className="flex-center text-primary gap-5">{/* Social Icons */}</div>
+                        <span className="text-secondary mb-3 mt-8">or use your email account</span>
+                        <IconInput groupClass="my-2 w-3/4" type="email" placeholder="Email" />
+                        <IconInput groupClass="my-2 w-3/4" type="password" placeholder="Password" />
+                        <a className="text-primary mb-8 mt-6 border-b border-solid border-b-[#a0a5a8] text-[15px]" href="#">
+                            Forgot your password?
+                        </a>
+                        <button className="neu-btn mt-12" type="button">
+                            SIGN IN
+                        </button>
+                    </form>
+                </div>
 
-                            <div>
-                                <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-                                    Password
-                                </label>
-                                <Field
-                                    type="password"
-                                    id="password"
-                                    name="password"
-                                    className="mt-1 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
-                                />
-                                <ErrorMessage name="password" component="div" className="text-red-500 text-sm mt-1" />
-                            </div>
+                {/* Switch Container */}
+                <div
+                    className="bg-primary absolute left-0 top-0 z-[200] flex h-full w-2/5 items-center justify-center overflow-hidden p-[50px] shadow-neu-light-md transition-all duration-[1.25s] dark:shadow-neu-dark-md"
+                    ref={switchContainerRef}>
+                    {/* Background Circles for Animation */}
+                    <div className="switch-circles bg-primary size-[500px] absolute bottom-[-60%] left-[-60%] rounded-full shadow-neu-inset-light-sm transition-all duration-[1.25s] dark:shadow-neu-inset-dark-sm"></div>
+                    <div className="switch-circles bg-primary size-[350px] absolute bottom-[-60%] left-[-60%] top-[-30%] rounded-full shadow-neu-inset-light-sm transition-all duration-[1.25s] dark:shadow-neu-inset-dark-sm"></div>
 
-                            <button
-                                type="submit"
-                                className="w-full bg-indigo-600 text-white py-2 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                                Login
-                            </button>
+                    {/* Login */}
+                    <div
+                        className="px-13 absolute flex w-full flex-col items-center justify-center py-12 transition-all duration-[1.25s]"
+                        ref={switchSignInRef}>
+                        <h2 className="text-primary mb-8 text-3xl font-bold">Welcome Back!</h2>
+                        <p className="text-secondary px-2 text-center text-sm leading-relaxed tracking-wide">
+                            To keep connected with us please login with your personal info.
+                        </p>
+                        <button className="neu-btn mt-12" onClick={toggleForm}>
+                            SIGN IN
+                        </button>
+                    </div>
 
-                            <div className="text-center mt-4">
-                                <Link to="/" className="text-indigo-600 hover:text-indigo-500">
-                                    Forgot your password?
-                                </Link>
-                            </div>
-                        </Form>
-                    )}
-                </Formik>
+                    {/* Register */}
+                    <div
+                        className="px-13 invisible absolute flex w-full flex-col items-center justify-center py-12 opacity-0 transition-all duration-[1.25s]"
+                        ref={switchSignUpRef}>
+                        <h2 className="text-primary mb-8 text-3xl font-bold">Hello Friend!</h2>
+                        <p className="text-secondary px-2 text-center text-sm leading-relaxed tracking-wide">
+                            Enter your personal details and start your journey with us.
+                        </p>
+                        <button className="neu-btn mt-12" onClick={toggleForm}>
+                            SIGN UP
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     )
